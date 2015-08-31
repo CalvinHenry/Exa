6e655a669955a887925ae407ba9590b5a3ad971a
CalvinHenry <calvinhenry60@gmail.com>
2015-08-31 14:21:16 -0400

    Added rotation stuff to Entity

--- Exa/src/Entity.java
+++ Exa/src/Entity.java
@@ -1,13 +1,23 @@
 import java.awt.Point;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
 
 public abstract class Entity {
 	
 	private Point location;
 	private Point momentum;
+	private AffineTransform transform;
+	private BufferedImage image;
+	
+	public Entity(){
+		
+	}
 	
 	public void updateLocation(){ //moves object according to momentum vector
 		location.x += momentum.x *Math.cos(momentum.y);
 		location.y += momentum.x *Math.sin(momentum.y);
+		updateTransform();
 	}
 	public void accelerate(double change){
 		momentum.x += change;
@@ -19,7 +29,15 @@ public abstract class Entity {
 		momentum.y += change;
 		while(momentum.y >= 360)
 			momentum.y -= 360;
+		updateTransform();
 	}
 	
+	private void updateTransform(){
+		transform = AffineTransform.getRotateInstance(momentum.y,location.x,location.y);
+	}
+	public BufferedImage getImage(){
+		AffineTransformOp op = new AffineTransformOp(transform, AffineTransformOp.TYPE_BILINEAR);
+		return  op.filter(image, null);
+	}
 	
 }
