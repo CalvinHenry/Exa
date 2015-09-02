5e90e8db3c24e2d75581a46a651130d7ad05d736
christophercoraggio <chriscoraggio1@gmail.com>
2015-09-02 14:25:58 -0400

    Added GUI1 with KeyListeners

---
+++ Exa/src/GUI1.java
@@ -0,0 +1,37 @@
+import java.awt.event.KeyEvent;
+
+public class GUI1 extends javax.swing.JFrame implements java.awt.event.KeyListener{
+
+	private static final long serialVersionUID = 1L;
+	
+	public GUI1(){
+		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+	}
+
+	public void keyPressed(KeyEvent e) {
+	    int keyCode = e.getKeyCode();
+	    switch( keyCode ) { 
+	        case KeyEvent.VK_UP:
+	            // handle up 
+	            break;
+	        case KeyEvent.VK_DOWN:
+	            // handle down 
+	            break;
+	        case KeyEvent.VK_LEFT:
+	            // handle left
+	            break;
+	        case KeyEvent.VK_RIGHT :
+	            // handle right
+	            break;
+	     }
+	} 
+
+	public void keyReleased(KeyEvent e) {
+		
+	}
+
+	public void keyTyped(KeyEvent e) {
+		
+	}
+
+}
