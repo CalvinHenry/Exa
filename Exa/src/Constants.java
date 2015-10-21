723e7b0ea61a4446393fc693f911167c6889a66d
christophercoraggio <chriscoraggio1@gmail.com>
2015-10-21 14:39:00 -0400

    Constants readjusted

--- Exa/src/Constants.java
+++ Exa/src/Constants.java
@@ -9,16 +9,24 @@ import javax.imageio.ImageIO;
  */
 public class Constants {
 	
+	public static BufferedImage[] images = new BufferedImage[5];
+	
+	/*
+	 * 0 - Qufeb
+	 * 1 - 
+	 * 2 - 
+	 */
+	
+	public static void initializeImages() throws java.io.IOException{
+		images[0] = ImageIO.read(new File(System.getProperty("user.home") + "/Desktop/Exa/Qufeb.png"));
+	}
+	
 	public static class Socket {
 		public static final int UPDATE_TIME = 10;
 		public static final int SERVER_REFRESH = 150;
 		public static final int CLIENT_REFRESH = 140;
-		public static final int CYCLES_TO_SERVER_UPDATE = SERVER_REFRESH / UPDATE_TIME; //The number of times the client refreshes the screen before the server sends an update
-		
+		public static final int CYCLES_TO_SERVER_UPDATE = SERVER_REFRESH / (2 * UPDATE_TIME); //The number of times the client refreshes the screen before the server sends an update
 	}
-	
-	BufferedImage[] images = new BufferedImage[5]; //5 images to load
-	//images[0] =
 	public static ArrayList<Message> entityToMessage(java.util.List<Entity> list){
 		ArrayList<Message> retrn = new ArrayList<>();
 		for(int i = 0; i < list.size(); i ++){
@@ -33,10 +41,5 @@ public class Constants {
 			retrn.add(new Entity(list.get(i)));
 		}
 		return retrn;
-	}
-	
-	public static BufferedImage getImage(String imagetype) throws java.io.IOException{
-		if(imagetype.equals("Qufeb")) return ImageIO.read(new File((System.getProperty("user.home") + "/Desktop/Exa/Qufeb.png")));
-		return null;
 	}
 }
